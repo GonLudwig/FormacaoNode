53a27114b05725f176cbded1b55b8dcb6f2f3cde
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-02-14 17:46:09 -0300

    create routes to study about params

--- app/Express1/index.js
+++ app/Express1/index.js
@@ -1,8 +1,24 @@
+const { response } = require("express");
 const express = require("express")
 const app =  express();
 
 app.get("/",(request, response) =>{
-    response.send("Bem vindo ao guia do programador")
+    response.send("Bem vindo ao guia do programadorrrrr")
+})
+
+app.get("/blog/:artigo?", (req, res) => {
+    let artigo = req.params.artigo
+
+    if (artigo) {
+        res.send("Com parametro artigo: "+ artigo)
+    }else {
+        res.send("Bem vindo ao meu blog")
+    }
+})
+
+app.get("/ola/:nome", (req, res) => {
+    let nome = req.params.nome
+    res.send("<h1>"+ nome + "</h1>")
 })
 
 app.listen(4000, (error) => {
