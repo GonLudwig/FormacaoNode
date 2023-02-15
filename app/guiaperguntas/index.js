91ee17fbdbdb1f2d9bd132328ee17b5bc66feac2
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-02-15 17:02:37 -0300

    add connection with database, Routes to form pergunta.

--- app/guiaperguntas/index.js
+++ app/guiaperguntas/index.js
@@ -1,10 +1,43 @@
 const express = require("express")
+const bodyParse = require("body-parser")
 const app = express()
+const connection = require("./database/database")
+const Pergunta = require("./database/Pergunta")
+
+connection
+    .authenticate()
+    .then(() =>{
+        console.log("Conectado com sucesso!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    })
 
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+app.use(bodyParse.urlencoded({extended: false}))
+app.use(bodyParse.json())
+
 app.get("/", (req, res) => {
     res.render("index")
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/")
+    })
+
 })
 
 app.listen(3000, () => {console.log("Aplicacao rodando com sucesso!")})
