4589812a672060695fb8cb9b6735b317d32fedf9
GlaucoStarling <glaucox.x@gmail.com>
2023-02-20 08:14:10 -0300

    Add categories in new aticles view

--- app/guiapress/articles/ArticlesController.js
+++ app/guiapress/articles/ArticlesController.js
@@ -1,13 +1,16 @@
 const { Router } = require("express")
 const express = require("express")
 const router = express.Router()
+const Category = require("../categories/Category")
 
 router.get("/articles", (req, res) => {
     res.send("Article route")
 })
 
-router.post("/admin/articles/new", (req, res) => {
-    res.send("New Article route")
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories})
+    })
 })
 
 module.exports = router
