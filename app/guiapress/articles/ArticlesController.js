0ecacaa93a74d5efd3f9a91375423d314de3ecc0
GlaucoStarling <glaucox.x@gmail.com>
2023-02-20 17:35:35 -0300

    add routes Home, save and delete to articles

--- app/guiapress/articles/ArticlesController.js
+++ app/guiapress/articles/ArticlesController.js
@@ -2,15 +2,55 @@ const { Router } = require("express")
 const express = require("express")
 const router = express.Router()
 const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
-router.get("/articles", (req, res) => {
-    res.send("Article route")
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("admin/articles/index", {articles: articles})
+    })
 })
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories})
     })
+})
+
+router.post("/articles/save", (req, res) => {
+    let title = req.body.title
+    let body = req.body.body
+    let categoryId = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: categoryId
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+
+})
+
+router.post("/articles/delete", (req, res) => {
+    let id = req.body.id
+
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            return Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            })
+        }
+    }
+
+    res.redirect("/admin/articles")
 })
 
 module.exports = router
