4788ae2daf5890ca8fd619d5196a9dd89fabf607
GlaucoStarling <glaucox.x@gmail.com>
2023-02-21 13:30:20 -0300

    remake default routes

--- app/guiapress/index.js
+++ app/guiapress/index.js
@@ -24,14 +24,61 @@ app.use(bodyParser.urlencoded({extented: false}))
 
 app.use(bodyParser.json())
 
+app.get("/", (req, res) => {
+    Article.findAll({
+        orderBy: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
+        Category.findAll({
+            orderBy: [
+                ['title', 'ASC']
+            ]
+        }).then( categories => {
+            res.render("index", {articles: articles, categories: categories})
+        })
+    })
+})
+
+app.get("/category/:slug", (req, res) => {
+    let slug = req.params.slug
+
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then( categories => {
+                res.render("index", {articles: category.articles, categories: categories})
+            })
+        }else{
+            res.redirect("/")
+        }
+    })
+})
+
+app.get("/:slug", (req, res) => {
+    let slug = req.params.slug
+
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if (article != undefined) {
+            Category.findAll().then( categories => {
+                return res.render("article", {article: article, categories: categories})
+            })
+        }
+
+        res.redirect("/")
+    })
+})
+
 app.use("/", articlesController)
 
 app.use("/", categoriesController)
-
-app.get("/", (req, res) => {
-    Article.findAll().then(articles => {
-        res.render("index", {articles: articles})
-    })
-})
 
 app.listen(3000, () => {console.log("Aplicação ativa")})
