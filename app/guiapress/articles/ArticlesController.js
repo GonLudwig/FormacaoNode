a34f0c08090851c371e96ffad5ae3c0efc310fc2
GlaucoStarling <glaucox.x@gmail.com>
2023-02-21 14:47:05 -0300

    Create edit route and viewing for articles

--- app/guiapress/articles/ArticlesController.js
+++ app/guiapress/articles/ArticlesController.js
@@ -19,6 +19,28 @@ router.get("/admin/articles/new", (req, res) => {
     })
 })
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    let id = req.params.id
+    if (isNaN(id)) {
+        return res.redirect("/admin/articles")
+    }
+    
+    Article.findByPk(id).then(article => {
+        console.log(article)
+        if(article != undefined) {
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article: article, categories: categories})
+            })
+        }else {
+            res.redirect("/admin/articles")
+        }
+
+    }).catch((error) => {
+        console.log(error)
+    })
+})
+
+
 router.post("/articles/save", (req, res) => {
     let title = req.body.title
     let body = req.body.body
@@ -52,5 +74,27 @@ router.post("/articles/delete", (req, res) => {
 
     res.redirect("/admin/articles")
 })
+
+router.post("/articles/update", (req, res) => {
+    let id = req.body.id
+    let title = req.body.title
+    let body = req.body.body
+    let categoryId = req.body.category
+
+    Article.update({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: categoryId
+    }, {
+        where: {id: id}
+    }).then(() => {
+        res. redirect("/admin/articles")
+    })
+})
+
+// router.get("/articles/page/:num", (req, res) => {
+
+// })
 
 module.exports = router
