1a8aab5526e894d0f9864ffaee030eb6cd28d182
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-02-23 16:12:20 -0300

    feat: create route for create user

--- app/guiapress/users/UsersController.js
+++ app/guiapress/users/UsersController.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcryptjs")
+const User = require("./User")
 
 router.get("/admin/users", (req, res) => {
     res.send("listagem de Usarios")
@@ -7,6 +9,21 @@ router.get("/admin/users", (req, res) => {
 
 router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create")
+})
+
+router.post("/users/create", (req, res) => {
+    let email = req.body.email
+    let salt = bcrypt.genSaltSync(10)
+    let password = bcrypt.hashSync(req.body.password, salt)
+
+    User.create({
+        email: email,
+        password: password
+    }).then(() => {
+        res.redirect("/")
+    }).catch(error => {
+        res.json(error)
+    })
 })
 
 module.exports = router
