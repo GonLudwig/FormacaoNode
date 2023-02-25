891088b67c256f5c3f00fc03749d6141fc90d409
GlaucoStarling <glaucox.x@gmail.com>
2023-02-25 16:54:21 -0300

    feat(usersController): create middleware for routes.

--- app/guiapress/users/UsersController.js
+++ app/guiapress/users/UsersController.js
@@ -2,16 +2,17 @@ const express = require("express")
 const router = express.Router()
 const bcrypt = require("bcryptjs")
 const User = require("./User")
+const adminAuth = require("../middlewares/adminAuth")
 
-router.get("/admin/users", (req, res) => {
+router.get("/admin/users", adminAuth, (req, res) => {
     res.send("listagem de Usarios")
 })
 
-router.get("/admin/users/create", (req, res) => {
+router.get("/admin/users/create", adminAuth, (req, res) => {
     res.render("admin/users/create")
 })
 
-router.post("/users/create", (req, res) => {
+router.post("/users/create", adminAuth, (req, res) => {
     let email = req.body.email
     
     User.findOne({where: {email: email}}).then( user => {
@@ -34,4 +35,34 @@ router.post("/users/create", (req, res) => {
 
 })
 
+router.get("/login", (req, res) => {
+    res.render("admin/users/login")
+})
+
+router.post("/authenticate", (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+
+    User.findOne({where: {email: email}}).then(user => {
+        if (user != undefined) {
+            let validation = bcrypt.compareSync(password, user.password)
+            if (validation) {
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
+                }
+                res.redirect("/admin/articles")
+            } else {
+                res.redirect("/login")    
+            }
+        } else {
+            res.redirect("/login")
+        }
+    })
+})
+
+router.get("/logout", (req, res) => {
+    req.session.user = undefined
+    res.redirect("/")
+})
 module.exports = router
