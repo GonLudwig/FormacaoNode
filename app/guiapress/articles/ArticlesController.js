86db73b2c29c3acf6df69f7a0c1a9020b730392c
GlaucoStarling <glaucox.x@gmail.com>
2023-02-25 16:42:01 -0300

    feat(ArticlesController): add middleware in routes

--- app/guiapress/articles/ArticlesController.js
+++ app/guiapress/articles/ArticlesController.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const Category = require("../categories/Category")
 const Article = require("./Article")
 const slugify = require("slugify")
+const adminAuth = require("../middlewares/adminAuth")
 
-router.get("/admin/articles", (req, res) => {
+router.get("/admin/articles", adminAuth, (req, res) => {
     Article.findAll({
         include: [{model: Category}]
     }).then(articles => {
@@ -13,13 +14,13 @@ router.get("/admin/articles", (req, res) => {
     })
 })
 
-router.get("/admin/articles/new", (req, res) => {
+router.get("/admin/articles/new", adminAuth, (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories})
     })
 })
 
-router.get("/admin/articles/edit/:id", (req, res) => {
+router.get("/admin/articles/edit/:id", adminAuth, (req, res) => {
     let id = req.params.id
     if (isNaN(id)) {
         return res.redirect("/admin/articles")
@@ -41,7 +42,7 @@ router.get("/admin/articles/edit/:id", (req, res) => {
 })
 
 
-router.post("/articles/save", (req, res) => {
+router.post("/articles/save", adminAuth, (req, res) => {
     let title = req.body.title
     let body = req.body.body
     let categoryId = req.body.category
@@ -57,7 +58,7 @@ router.post("/articles/save", (req, res) => {
 
 })
 
-router.post("/articles/delete", (req, res) => {
+router.post("/articles/delete", adminAuth, (req, res) => {
     let id = req.body.id
 
     if (id != undefined) {
@@ -75,7 +76,7 @@ router.post("/articles/delete", (req, res) => {
     res.redirect("/admin/articles")
 })
 
-router.post("/articles/update", (req, res) => {
+router.post("/articles/update", adminAuth, (req, res) => {
     let id = req.body.id
     let title = req.body.title
     let body = req.body.body
