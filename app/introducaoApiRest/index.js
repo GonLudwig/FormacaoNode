d2883e04db2b8bb662937756f408dc09cc203e9a
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-03-03 11:35:51 -0300

    feat: Crete post, delete and put methods

--- app/introducaoApiRest/index.js
+++ app/introducaoApiRest/index.js
@@ -33,4 +33,61 @@ app.get("/games/:id", (req, res) => {
     }
 })
 
+app.post("/games", (req, res) => {
+    let {titulo, ano, preco} = req.body
+
+    DB.games.push({
+        id: (+DB.games.length) + 1,
+        titulo: titulo,
+        ano: ano,
+        preco: preco
+    })
+
+    res.sendStatus(200)
+})
+
+app.delete("/games/:id", (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)
+    } else {
+        let id = req.params.id
+        let index = DB.games.findIndex(game => id === game.id)
+        if (index >= 0) {
+            DB.games.splice(id,1)
+            res.sendStatus(200)
+        } else {
+            res.sendStatus(404)
+        }
+    }
+})
+
+app.put("/games/:id", (req, res) => {
+    let id = req.params.id
+
+    if (isNaN(id)) {
+        res.sendStatus(400)
+    } else {
+        id = parseInt(id)
+
+        let game = DB.games.find(game => game.id == id)
+
+        if (game == undefined) {
+            res.sendStatus(404)
+        }else {
+            let {title, price, year} = req.body
+
+            if (title != undefined) {
+                game.title = title
+            }
+            if (price != undefined) {
+                game.price = price
+            }
+            if (year != undefined) {
+                game.year = year
+            }
+            res.sendStatus(201)
+        }
+    }
+})
+
 app.listen(3000, () => console.log("Servidor Ligado!"))
